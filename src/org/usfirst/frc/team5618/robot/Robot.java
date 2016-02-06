87e959415c31efeab13f929d8c834ef70871864f
Nicolas Brideau <nbrideau@csbf.qc.ca>
2016-02-06 11:14:13 -0500

    the cake is a lie

--- src/org/usfirst/frc/team5618/robot/Robot.java
+++ src/org/usfirst/frc/team5618/robot/Robot.java
@@ -36,8 +36,6 @@ public class Robot extends IterativeRobot {
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
-        
-        
     }
 	
 	/**
