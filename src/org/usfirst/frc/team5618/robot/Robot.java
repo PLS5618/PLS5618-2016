adedd5314f8d721d6a9790600096d6bcaaa48005
Anthony Marcoux <anthonynoireaux2002@gmail.com>
2016-02-08 19:01:21 -0500

    sa marche

--- src/org/usfirst/frc/team5618/robot/Robot.java
+++ src/org/usfirst/frc/team5618/robot/Robot.java
@@ -102,6 +102,7 @@ public class Robot extends IterativeRobot {
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
+    	
         if (autonomousCommand != null) autonomousCommand.cancel();
     }
 
